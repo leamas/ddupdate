f05620e54c9e270c0906386e32fac2b8eb6e1112
Alec Leamas <leamas.alec@gmail.com>
2018-02-03 20:22:39 +0100

    build: Don't install documentation files.

--- setup.py
+++ setup.py
@@ -19,12 +19,6 @@ DATA = [
     ('/lib/systemd/system', glob('systemd/*')),
     ('share/man/man8', ['ddupdate.8', 'ddupdate-config.8']),
     ('share/man/man5', ['ddupdate.conf.5']),
-    ('share/doc/ddupdate',
-        ['CONFIGURATION.md',
-         'CONTRIBUTE.md',
-         'README.md',
-         'LICENSE.txt',
-         'NEWS']),
     ('share/ddupdate/dispatcher.d', ['dispatcher.d/50-ddupdate'])
 ]
 
