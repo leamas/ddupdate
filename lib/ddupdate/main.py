eb6ef61be54a0aab1a964d00431b652c54210d85
Alec Leamas <leamas.alec@gmail.com>
2019-06-07 09:37:35 +0200

    Preparing 0.6.3

--- lib/ddupdate/main.py
+++ lib/ddupdate/main.py
@@ -236,7 +236,7 @@ def parse_options(conf):
         'debug': logging.DEBUG,
     }
     parser = get_parser(conf)
-    parser.version = "0.6.2"
+    parser.version = "0.6.3"
     opts = parser.parse_args()
     if opts.help == '-':
         parser.print_help()
