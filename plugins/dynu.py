4446f12a3acb160914c33fec477d9808b7e85110
Alec Leamas <leamas.alec@nowhere.net>
2022-04-04 19:10:38 +0200

    duny.com: Update to handle new service API.

--- plugins/dynu.py
+++ plugins/dynu.py
@@ -5,8 +5,11 @@ See: ddupdate(8)
 See: https://www.dynu.com/Resources/API/Documentation
 
 """
-import hashlib
-from ddupdate.ddplugin import ServicePlugin, get_response, get_netrc_auth
+import base64
+from urllib.parse import urlencode, urlparse
+
+from ddupdate.ddplugin import ServicePlugin, ServiceError
+from ddupdate.ddplugin import get_response, get_netrc_auth
 
 
 class DynuPlugin(ServicePlugin):
@@ -21,20 +24,26 @@ class DynuPlugin(ServicePlugin):
 
     Options:
         none
+
+    See: https://www.dynu.com/en-US/DynamicDNS/IP-Update-Protocol
     """
 
     _name = 'dynu.com'
     _oneliner = 'Updates on https://www.dynu.com/en-US/DynamicDNS'
-    _url = "https://api.dynu.com" \
-        + "/nic/update?hostname={0}&username={1}&password={2}"
+    _url = "https://api.dynu.com/nic/update?host={0}"
 
     def register(self, log, hostname, ip, options):
         """Implement ServicePlugin.register()."""
-        user, password = get_netrc_auth('api.dynu.com')
-        pw_hash = hashlib.md5(password.encode()).hexdigest()
-        url = self._url.format(hostname, user, pw_hash)
+        url = self._url.format(hostname)
+        api_host = urlparse(url).hostname
+        username, password = get_netrc_auth(api_host)
+        user_pw = ('%s:%s' % (username, password))
+        credentials = base64.b64encode(user_pw.encode('ascii'))
+        auth_header = ('Authorization', 'Basic ' + credentials.decode("ascii"))
         if ip and ip.v4:
             url += "&myip=" + ip.v4
         if ip and ip.v6:
             url += "&myipv6=" + ip.v6
-        get_response(log, url)
+        reply = get_response(log, url, header = auth_header)
+        if not ('good' in reply or 'nochg' in reply):
+            raise ServiceError("Update error: " + reply)
