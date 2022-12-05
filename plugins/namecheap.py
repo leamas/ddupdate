bec4e368643965dae61ede79883895e636924004
timball <timball@gmail.com>
2022-12-05 13:46:37 -0500

    update namecheap.py to not use xmltodict and use xml.etree instead

--- plugins/namecheap.py
+++ plugins/namecheap.py
@@ -7,8 +7,7 @@ See: https://www.namecheap.com/support/knowledgebase/article.aspx/29/11/how-to-d
 """
 from ddupdate.ddplugin import ServicePlugin, ServiceError
 from ddupdate.ddplugin import get_response, get_netrc_auth
-import xmltodict
-
+import xml.etree.ElementTree as ET
 
 class NamecheapPlugin(ServicePlugin):
     """
@@ -32,13 +31,22 @@ class NamecheapPlugin(ServicePlugin):
     _oneliner = 'Updates on http://namecheap.com'
     _url = "https://dynamicdns.park-your-domain.com/update?host={0}&domain={1}&ip={2}&password={3}"
 
+    def _etree_to_dict(self, t):
+        """ https://stackoverflow.com/questions/7684333/converting-xml-to-dictionary-using-elementtree/68082847#68082847 """
+        if type(t) is ET.ElementTree: return self._etree_to_dict(t.getroot())
+        return {
+            **t.attrib,
+            'text': t.text,
+            **{e.tag: self._etree_to_dict(e) for e in t}
+        }
+
     def register(self, log, hostname, ip, options):
         """Implement ServicePlugin.register()."""
 
         password = get_netrc_auth(self._name)[1]
         # domain is last two elements of 'host'
         domain = '.'.join(hostname.split('.')[-2:])
-        # hostname is everying above the last two elements of 'host'
+        # hostname is everything above the last two elements of 'host'
         host = '.'.join(hostname.split('.')[:-2])
         # ip address is either an ipv4 or ipv6
         ip = ip.v4 if ip.v4 else ip.v6
@@ -46,6 +54,8 @@ class NamecheapPlugin(ServicePlugin):
         url = self._url.format(host, domain, ip, password)
         html = get_response(log, url)
 
-        resp = xmltodict.parse(html)['interface-response']
-        if resp['errors'] is not None:
-            raise ServiceError("Update error, got: " + resp['errors'])
+        tree = ET.ElementTree(ET.fromstring(html))
+        resp = self._etree_to_dict(tree)
+
+        if resp['errors']['text'] is not None:
+            raise ServiceError("Update error, got: " + resp['errors']['text'])
